8768d620d76d17ad8e23d7c974218f2eac22a821
jlmack13 <jennimacklin@gmail.com>
2018-09-11 11:16:05 -0400

    moved checkCharacters function to top for clarity and added brief description

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+//Given a string consisting of some combination of parentheses, brackets, and braces,
+//return true if they're properly nested and balanced, and false otherwise.  
+
+function checkCharacters(string) {
+  let arr = [];
+  //check for illegal characters
+  if(string.match(/[^ ()[\]{}]/g)) {
+    return "Error: Invalid character detected. Please try again using only '()[]{}' characters.";
+  }
+  //remove white space from string
+  let str = string.replace(/ /g,'');
+  for(let char of str) {
+    //check for opening char and push into array if true
+    if (isOpen(char)) {
+      arr.push(char)
+    } 
+    //check for a match - use char on top of stack as first arg and current char as the second arg
+    else if (isMatch(arr[arr.length -1], char)) {
+      arr.pop();
+    }
+    //check the top of the stack, if its empty or if its a non-matching char return false
+    else if (arr.length === 0 || !isMatch(arr[arr.length - 1], char)) {
+      return false;
+    } 
+  }
+  return arr.length === 0 ? true : false;
+}
+
 //function isOpen(c) - returns true if c === [, c ==={, or c===(, false otherwise
 function isOpen(char) {
   if(char != null) {
@@ -5,7 +33,6 @@ function isOpen(char) {
   } else {
     return false;
   }
-
 }
 
 //function isClosed(c) - returns true if c === ], c ===}, or c===), false otherwise
@@ -40,30 +67,6 @@ function isMatch(first, second) {
   }
 }
 
-function checkCharacters(string) {
-  let arr = [];
-  //check for illegal characters
-  if(string.match(/[^ ()[\]{}]/g)) {
-    return "Error: Invalid character detected. Please try again using only '()[]{}' characters.";
-  }
-  //remove white space from string
-  let str = string.replace(/ /g,'');
-	for(let char of str) {
-    //check for opening char and push into array if true
-		if (isOpen(char)) {
-	    arr.push(char)
-    } 
-    //check for a match - use char on top of stack as first arg and current char as the second arg
-    else if (isMatch(arr[arr.length -1], char)) {
-	    arr.pop();
-    }
-    //check the top of the stack, if its empty or if its a non-matching char return false
-    else if (arr.length === 0 || !isMatch(arr[arr.length - 1], char)) {
-	    return false;
-    } 
-  }
-  return arr.length === 0 ? true : false;
-}
 
 module.exports = checkCharacters;
 
